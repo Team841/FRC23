3e1ed590b66a932c6f593ee891c4267e0d024ed9
David Tian <82059858+NanoDecade@users.noreply.github.com>
2023-03-18 12:14:40 -0700

    somewhat working

---
+++ src/main/java/frc/robot/commands/coneOutTake.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.C;
+import frc.robot.subsystems.Superstructure;
+
+public class coneOutTake extends CommandBase {
+  /** Creates a new coneOutTake. */
+
+  private Superstructure c_Superstructure;
+  private double time = Timer.getFPGATimestamp();
+  public coneOutTake(Superstructure superstructure) {
+    c_Superstructure = superstructure;
+    addRequirements(superstructure);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    time = Timer.getFPGATimestamp();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    c_Superstructure.setIntakeMotor(C.Superstructure.IntakeMotorTalonPercentPower);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    c_Superstructure.setIntakeMotor(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return (Timer.getFPGATimestamp() > (time+3.0));
+  }
+}
