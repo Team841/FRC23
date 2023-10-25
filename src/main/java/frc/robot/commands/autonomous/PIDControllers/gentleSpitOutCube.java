9f96ed6c306c51a664449b4660ea3aae98552c2d
Restyled.io <commits@restyled.io>
2023-10-25 23:25:11 +0000

    Restyled by clang-format

--- src/main/java/frc/robot/commands/autonomous/PIDControllers/gentleSpitOutCube.java
+++ src/main/java/frc/robot/commands/autonomous/PIDControllers/gentleSpitOutCube.java
@@ -7,7 +7,8 @@ import frc.robot.subsystems.Superstructure;
 public class gentleSpitOutCube extends CommandBase {
   private Superstructure c_Superstructure;
   /** Creates a new SpitOutCone. */
-  //this file was made to spit out a cube with less power for auto. NOT COMPLETE
+  // this file was made to spit out a cube with less power for auto. NOT
+  // COMPLETE
   public gentleSpitOutCube(Superstructure superstructure) {
     c_Superstructure = superstructure;
     // Use addRequirements() here to declare subsystem dependencies.
@@ -17,27 +18,22 @@ public class gentleSpitOutCube extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    c_Superstructure.gentleSpitOutCube(); 
+    c_Superstructure.gentleSpitOutCube();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    c_Superstructure.setIntakeMotor(0);// stop the intake when its done
+    c_Superstructure.setIntakeMotor(0); // stop the intake when its done
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;// Stop the command on the w ait command
-
-
-
+    return false; // Stop the command on the w ait command
   }
 }
